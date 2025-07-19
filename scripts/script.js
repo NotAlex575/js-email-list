075c43965a0e3cdb87a70ece6858349b89a74970
NotAlex575 <dapontealessandro89gmail.com>
2025-07-19 16:46:25 +0200

    modificato ciclo for per permettere poi di creare direttamente nel javascript delle card li all'interno dell'ol

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,38 @@ const email_list = document.getElementById("lista-email");
 
 const emails = [];
 
+//ESECUZIONE RENDER CARD LIST
+const renderCards = () =>{
+  for(let i = 0; i < emails.length; i++){
+    let card = generateCard(emails[i]);
+    email_list.innerHTML += card;
+  }
+}
 
+const generateCard = (email) => {
+  return `<li class="list-group-item">${email}</li>`;
+}
+
+//CREAZIONE EMAIL E CARD LIST
+
+let counter_email = 0;
 
 for(let i = 0; i < 10; i++){
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(response => {
-      const email = response.data.response;
-      emails[i] = email;
+        const email = response.data.response;
+        emails[i] = email;
+        counter_email++;
+        if(counter_email == 10){
+            renderCards();
+        }
     })
 }
 
-console.log(emails);
+
+
+
+
+
 
 
 
